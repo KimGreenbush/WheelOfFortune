b453bba27674c08c38865cd44db6198bf7603614
v-tmackin <v-tmackin@microsoft.com>
2021-09-14 15:20:03 -0700

    Add project files.

---
+++ Wheel_Of_Fortune/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wheel_Of_Fortune
+{
+    public class TestClass
+    {
+        public int Add(int x, int y) => x + y;
+    }
+}
