8fba9171bd74fd24db7f5bc282002cea95179815
Kimberley Greenbush <v-kgreenbush@microsoft.com>
2021-09-15 13:58:09 -0700

    add test method outlines

---
+++ Wheel_Of_Fortune_Testing/GuessTests.cs
@@ -0,0 +1,63 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wheel_Of_Fortune;
+using Moq;
+
+namespace Wheel_Of_Fortune_Testing
+{
+    [TestClass]
+    public class GuessTests
+    {
+        string samplePuzzleNoSpaces = "Word";
+        string samplePuzzleWithSpaces = "Some words";
+        string correctSampleLetter = "w";
+        string incorrectSampleLetter = "a";
+
+        [TestMethod]
+        public void TestGuessLetterNoSpaces()
+        {
+           //Arrange
+           // need a sample puzzle, letter
+           
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void TestSolvePuzzleNoSpaces()
+        {
+            //Arrange
+            // need a sample puzzle, solution
+            //
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void TestGuessLetterWithSpaces()
+        {
+            //Arrange
+            // need a sample puzzle, letter
+            //
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void TestSolvePuzzleWithSpaces()
+        {
+            //Arrange
+            // need a sample puzzle, solution
+            //
+
+            // Act
+
+            // Assert
+        }
+    }
+}
