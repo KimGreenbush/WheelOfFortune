c563fca8f2e9779e8af45e8f69cd88971642bd0f
Kimberley Greenbush <v-kgreenbush@microsoft.com>
2021-09-15 14:48:29 -0700

    add bool method to check for alpha only

--- Wheel_Of_Fortune/Guess.cs
+++ Wheel_Of_Fortune/Guess.cs
@@ -1,19 +1,25 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace Wheel_Of_Fortune
 {
     public static class Guess : IGuess
     {
+        bool isAlpha(string guess)
+        {
+            return Regex.IsMatch( guess, @"^[a-zA-Z]+$" );
+        }
+
         public void GuessLetter(string letter)
         {
-            // pass letter to Puzzle => return string letter
+            // pass letter to Puzzle => return bool
             // true print "Correct letter"
             // false print "Incorrect letter"
         }
 
         public void SolvePuzzle(string attemptedSolution)
         {
-            // pass attemptedSolution to Puzzle => return string attemptedSolution
+            // pass attemptedSolution to Puzzle => return bool
             // true print "Correct! You win!"
             // false print "Incorrect solution"
         }
