e00b60d856fd3070faa84051c38a517e931f8cf6
Kimberley Greenbush <v-kgreenbush@microsoft.com>
2021-09-16 13:50:28 -0700

    add summary prompts to methods

--- Wheel_Of_Fortune/Guess.cs
+++ Wheel_Of_Fortune/Guess.cs
@@ -3,31 +3,55 @@ using System.Text.RegularExpressions;
 
 namespace Wheel_Of_Fortune
 {
-    public static class Guess : IGuess
+    public static class Guess
     {
-        bool isAlpha(string guess)
+        private static bool isCorrect = false;
+
+        /// <summary>
+        /// Checks that the input value only contains alpha characters (letters).
+        /// </summary>
+        /// <param name="guess"></param>
+        /// <returns></returns>
+        static bool IsAlpha(string guess)
         {
-            return Regex.IsMatch( guess, @"^[a-zA-Z]+$" );
+            bool isOnlyLetters = Regex.IsMatch( guess, @"^[a-zA-Z]+$" );
+
+            if ( !isOnlyLetters )
+            {
+                throw new Exception( "Guess should only contain letters." );
+            }
+            return isOnlyLetters;
+          
         }
 
-        public void GuessLetter(string letter)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="letter"></param>
+        /// <returns></returns>
+        public static bool GuessLetter(string letter)
         {
-            // pass letter to Puzzle => return bool
+            // check isAlpha
+            // pass letter to Puzzle CheckLetter(string) => return bool
             // true print "Correct letter"
             // false print "Incorrect letter"
+            // isCorrect = response from Puzzle
+            return isCorrect;
         }
 
-        public void SolvePuzzle(string attemptedSolution)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="attemptedSolution"></param>
+        /// <returns></returns>
+        public static bool SolvePuzzle(string attemptedSolution)
         {
-            // pass attemptedSolution to Puzzle => return bool
+            // check isAlpha
+            // pass attemptedSolution to Puzzle SolveProblem(string) => return bool
             // true print "Correct! You win!"
             // false print "Incorrect solution"
+            // isCorrect = response from Puzzle
+            return isCorrect;
         }
-    }
-
-    public interface IGuess
-    {
-        public void GuessLetter(string value) { }
-        public void SolvePuzzle(string value) { }
     }
 }
