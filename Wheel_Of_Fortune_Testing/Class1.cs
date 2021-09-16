9f2ab4631c2f5066632577f48c2722a2d1ea2385
Trang Nguyen <v-nguyent@microsoft.com>
2021-09-16 14:36:12 -0700

    Add first version that return funtion 3 funtion

---
+++ Wheel_Of_Fortune_Testing/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wheel_Of_Fortune_Testing
+{
+    class Class1
+    {
+    }
+}
