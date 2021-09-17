3d110ab2d92c002e43d13b0f1d67bd284b5c1675
Trang Nguyen <v-nguyent@microsoft.com>
2021-09-17 01:10:46 -0700

    change the return and add unit test

---
+++ Wheel_Of_Fortune/Puzzle/PuzzleObject.cs
@@ -0,0 +1,15 @@
+﻿namespace Wheel_Of_Fortune
+{
+    public class PuzzleObject 
+    {
+        public readonly string hint;
+        public readonly string currentStatusPuzzle;
+        public readonly string guessedLetter;
+        public PuzzleObject(string hint, string currentStatusPuzzle, string guessedLetter)
+        {
+            this.hint = hint;
+            this.currentStatusPuzzle = currentStatusPuzzle;
+            this.guessedLetter = guessedLetter;
+        }
+    }
+}
---
+++ Wheel_Of_Fortune_Testing/PuzzleControllerTest.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wheel_Of_Fortune;
+
+namespace Wheel_Of_Fortune_Testing
+{
+    [TestClass]
+    public class PuzzleControllerTest
+    {
+        [TestMethod]
+        public void GetPuzzleObject_FirstGuess_HappyCase()
+        {
+            // Arrange
+            PuzzleController puzzle = PuzzleController.GetInstance();
+            puzzle.GeneratePuzzle(0);
+            puzzle.CheckLetter("j");
+
+            // Act
+            PuzzleObject result = puzzle.GetPuzzleObject();
+
+            // Assert
+            Assert.AreEqual("steady run", result.hint);
+            Assert.AreEqual("j##", result.currentStatusPuzzle);
+            Assert.AreEqual("j", result.guessedLetter);
+
+        }
+
+        [TestMethod]
+        public void GetPuzzleObject_FirstGuessWrong_HappyCase()
+        {
+            // Arrange
+            PuzzleController puzzle = PuzzleController.GetInstance();
+            puzzle.GeneratePuzzle(0);
+            puzzle.CheckLetter("r");
+
+            // Act
+            PuzzleObject result = puzzle.GetPuzzleObject();
+
+            // Assert
+            Assert.AreEqual("steady run", result.hint);
+            Assert.AreEqual("###", result.currentStatusPuzzle);
+            Assert.AreEqual("r", result.guessedLetter);
+
+        }
+
+        [TestMethod]
+        public void CheckLetter_FirstGuessCorrect_HappyCase()
+        {
+            // Arrange
+            PuzzleController puzzle = PuzzleController.GetInstance();
+            puzzle.GeneratePuzzle(0);
+
+
+            // Act
+            bool result = puzzle.CheckLetter("j");
+
+            // Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void CheckLetter_FirstGuessWrong_HappyCase()
+        {
+            // Arrange
+            PuzzleController puzzle = PuzzleController.GetInstance();
+            puzzle.GeneratePuzzle(0);
+
+            // Act
+            bool result = puzzle.CheckLetter("r");
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void CheckLetter_CheckException_ThrowAgrumentException()
+        {
+            // Arrange
+            PuzzleController puzzle = PuzzleController.GetInstance();
+            puzzle.GeneratePuzzle(0);
+
+            // Act
+            try
+            {
+                bool result = puzzle.CheckLetter("re");
+            }
+            catch (System.ArgumentException ex)
+            {
+                Assert.AreEqual("invalid letter (Parameter 'letter')", ex.Message);
+                Assert.AreEqual("letter", ex.ParamName);
+            }
+            catch (System.Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void SolveProblem_GuessCorrect_HappyCase()
+        {
+            // Arrange
+            PuzzleController puzzle = PuzzleController.GetInstance();
+            puzzle.GeneratePuzzle(0);
+
+
+            // Act
+            bool result = puzzle.SolveProblem("jog");
+
+            // Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void SolveProblem_GuessWrong_HappyCase()
+        {
+            // Arrange
+            PuzzleController puzzle = PuzzleController.GetInstance();
+            puzzle.GeneratePuzzle(0);
+
+
+            // Act
+            bool result = puzzle.SolveProblem("fog");
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+    }
+}
